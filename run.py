e782c204ca1dff7ba3847a53a523e746034a8194
289725386@qq.com <289725386@qq.com>
2018-05-06 23:36:09 +0800

    debug

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import chrome
 import config
 import downloader
-import environment
 import util
 import nginx
 import web_api
@@ -25,26 +24,29 @@ nginx.nginx_config_init()
 nginx.nginx_reload_start()
 
 # start web api service
-web_api.instance.run(threaded=True)
+web_api.stater.start()
 
 # start web socket service
 web_socket.instance.start()
 
+wait_event = threading.Event()
+wait_event.wait()
+
 #check if service id is empty, and wifi is not available, ask to setup wifi
 config.const_service_id = util.get_cached_version(config.const_service_id_name)
 if config.const_service_id is None:
-    if wifi_checker.checker.is_network_available() == False:
+    if wifi_checker.instance.is_network_available() == False:
         chrome.instance.start('%s/Content/wifi.html'%(config.const_client_web_server_root))
-        while wifi_checker.checker.is_network_available() == False:
+        while wifi_checker.instance.is_network_available() == False:
             time.sleep(1)
 
 #check is service id is empty, ask user to register with mc code
 if config.const_service_id is None:
-    chrome.chrome_instance.start('%s/Content/register.html'%(config.const_client_web_server_root))
-    temp_serviceid = downloader.updater.get_service_id_from_remote(machine_code)
+    chrome.instance.start('%s/Content/register.html'%(config.const_client_web_server_root))
+    temp_serviceid = downloader.instance.get_service_id_from_remote(machine_code)
     while temp_serviceid is None or temp_serviceid == '':
         time.sleep(1)
-        temp_serviceid = downloader.updater.get_service_id_from_remote(machine_code)
+        temp_serviceid = downloader.instance.get_service_id_from_remote(machine_code)
     config.const_service_id = temp_serviceid
     util.set_cached_version(config.const_service_id_name,temp_serviceid)
 
@@ -55,10 +57,9 @@ downloader.instance.start()
 time.sleep(5)
 
 #reload for default start
-default_app = downloader.updater_instance.get_default_start()
-if default_app is not None
-    msg = 
-    {
+default_app = downloader.instance.get_default_start()
+if default_app is not None:
+    msg = {
         'MessageType':0,
         'MessageData':StartPath['StartPath']
     }
--- web_api.py
+++ web_api.py
@@ -8,6 +8,11 @@ import subprocess
 import config
 import uuid
 import os
+import threading
+import util
+import config
+import requests
+
 instance = Flask(__name__)
 # /opt/rsdis/config
 # /opt/rsdis/apps
@@ -18,21 +23,21 @@ instance = Flask(__name__)
 # web server
 
 
-@web_app_api.route("/api/contentInfos", methods=['GET'])
+@instance.route("/api/contentInfos", methods=['GET'])
 def contentInfos():
     data = json.load('%s/Content/AppContents/app_info.json' %
                      (config.const_client_root()))
     return data
 
 
-@web_app_api.route("/api/productInfos", methods=['GET'])
+@instance.route("/api/productInfos", methods=['GET'])
 def productInfos():
     data = json.load('%s/Content/ProductResources/data.js' %
                      (config.const_client_root()))
     return data
 
 
-@web_app_api.route("/api/qrByUnique/<unique>", methods=['GET'])
+@instance.route("/api/qrByUnique/<unique>", methods=['GET'])
 def qrByUnique(unique):
     ret_obj = {
         'Id': str(uuid.uuid1()),
@@ -43,7 +48,7 @@ def qrByUnique(unique):
     return json.dumps(ret_obj)
 
 
-@web_app_api.route("/api/qrInfos", methods=['GET'])
+@instance.route("/api/qrInfos", methods=['GET'])
 def qrInfos():
     ret = []
     for root, dirs, files in os.walk('%s/content/QrCodeResources/'):
@@ -59,16 +64,24 @@ def qrInfos():
     return json.dumps(ret)
 
 
-@web_app_api.route("/api/wifiSsids", methods=['GET'])
+@instance.route("/api/wifiSsids", methods=['GET'])
 def wifiSsids():
     return
 
 
-@web_app_api.route("/api/SetUpWifi/<ssid>/<pwd>", methods=['GET'])
+@instance.route("/api/SetUpWifi/<ssid>/<pwd>", methods=['GET'])
 def SetUpWifi(ssid, pwd):
     return
 
 
-@web_app_api.route("/api/RegisterCode", methods=['GET'])
+@instance.route("/api/RegisterCode", methods=['GET'])
 def RegisterCode():
+    url = '%s/%s/CreateDeviceActivationQrcode'%(util.util_remote_service(config.const_api_name_wechat),config.const_api_name_wechat)
+    data = util.get_cached_version('mc')
+    rsp = requests.post(url,json=).text()
     return
+
+def woker():
+    instance.run(threaded=True)
+
+stater = threading.Thread(target=woker)
