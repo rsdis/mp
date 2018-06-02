24b9313f162f0141905aaa23e5982d17f534254d
289725386@qq.com <289725386@qq.com>
2018-06-02 19:05:08 +0800

    new

--- pre_install.sh
+++ pre_install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 
+#------------------------------
+# readme, execute follow command
+# # wget -O install.sh https://raw.githubusercontent.com/rsdis/mp/master/pre_install.sh && sh install.sh && rm -f install.sh
+#
+#-----------------------------
+
+
+
+#read serviceid
+echo "============> Please input service id and type enter <============"
+read user_input_service_id
+echo "your input service id is "$user_input_service_id
+echo "=================================================================="
+
+
 #a. auto login, config
-
-
 #b. disable update, the solution is remove update manager
 sudo apt-get -y remove update-manager
-
 #c. set desktop background image
-
 #d. install chrom,python,nginx and python dependency
 sudo apt-get update
 sudo apt-get -y install curl
@@ -23,4 +34,16 @@ pip3.6 install websockets
 pip3.6 install python-etcd
 pip3.6 install requests
 
+#donload default application
+cd ~ 
+wget -O client.zip http://localhost:8080/Content/mydis.zip
+unzip -o client.zip -d ./fview
+rm -f client.zip
 
+#write service id
+rm -f  ./fview/dis/buildin/vers/service_id.ver
+echo $user_input_service_id >> ./fview/dis/buildin/vers/service_id.ver
+
+#start service
+cd ~/fview/dis/
+sh run.sh
