619b613c09a3ded5ef6140949cc8db27e3b7036a
x-man <x-man@future.go>
2018-06-04 22:57:59 +0800

    test

--- pre_install.sh
+++ pre_install.sh
@@ -20,7 +20,7 @@ sudo apt-get update
 sudo apt-get -y install python3.6
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3.6
 
-
+sudo apt-get -y install chromium-browser
 
 sudo apt-get -y install curl
 sudo apt-get -y install zip
