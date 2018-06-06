c21a441ff937b36355f8b6e0895a9639b5f60e54
Unknown <289725386@qq.com>
2018-06-06 09:43:33 +0800

    open chrome

--- chrome.py
+++ chrome.py
@@ -12,13 +12,13 @@ class chrome:
         while True:
             try:
                 subprocess.call(
-                    ['chromium-browser','--start-fullscreen', "--disk-cache-dir=/dev/null --disk-cache-size=1", '--app=' + self.start_url,'--user-data-dir=/opt/chromium-browser'])
+                    ['sudo','-i', '-u', 'fview', 'chromium-browser','--start-fullscreen', "--disk-cache-dir=/dev/null --disk-cache-size=1", '--app=' + self.start_url])
             except Exception as err:
                 print(err)
 
     def start(self, url):
         self.start_url = url
-        subprocess.call(['pkill', 'chromium-browse'])
+        subprocess.call(['pkill', 'chromium-browser'])
         self.woker_thread.start()
 
 
